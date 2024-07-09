dc2b80fc05391d9b7a84d4d43e9f17da88640d04
Annabelle Perbet <annabelleperbet@yahoo.fr>
2024-07-09 17:46:52 +0200

    Ajout du fichier script.js contenant la fonction sum

---
+++ script.js
@@ -0,0 +1,11 @@
+// Déclaration de la fonction de sum qui va permettre additionner deux nombres
+const sum = (a, b) => {
+  return a + b;
+};
+
+// Utilisation de la fonction et affichage du résultat dans la console
+const num1 = 24;
+const num2 = 100;
+const result = sum(num1, num2);
+
+console.log(`La somme de ${num1} et ${num2} est : ${result}`);
